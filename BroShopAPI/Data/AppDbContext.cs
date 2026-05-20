7f3fab95454f109e4cc164a7f6c22b0be7778308
joilchan <lizunov2077@mail.ru>
2026-05-20 23:36:33 +0300

    add d-c

--- BroShopAPI/Data/AppDbContext.cs
+++ BroShopAPI/Data/AppDbContext.cs
@@ -38,7 +38,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=BroShop;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=94.232.43.184;Persist Security Info=True;User ID=sa;Password=YourStrong@Pass123!;Pooling=False;Encrypt=True;TrustServerCertificate=True;Command Timeout=0");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
