9642cb83576a72776679c6a15b2f82dde6dc328b
joilchan <lizunov2077@mail.ru>
2026-06-23 05:45:59 +0300

    gggg

--- BroShopAPI/Models/Review.cs
+++ BroShopAPI/Models/Review.cs
@@ -12,7 +12,7 @@ public partial class Review
 
     public string Text { get; set; } = null!;
 
-    public decimal Rating { get; set; }
+    public double Rating { get; set; }
 
     public virtual Product Product { get; set; } = null!;
 
